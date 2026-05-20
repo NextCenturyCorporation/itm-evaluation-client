8c39c1941aa5b92f6cbb447eaba335ff85a4aef9
Darren Gemoets <darren.gemoets@caci.com>
2026-05-20 13:20:28 -0400

    Added support for AF-SS scenarios/targets

--- itm/itm_scenario_runner.py
+++ itm/itm_scenario_runner.py
@@ -27,6 +27,7 @@ adept_ss_alignment_targets = ['Feb2026-SS-4', 'Feb2026-SS-6']
 adept_ps_alignment_targets = ['Feb2026-PS-7', 'Feb2026-PS-3']
 adept_mf_af_alignment_targets = ['ADEPT-June2025-affiliation_merit-0.0_1.0', 'ADEPT-June2025-affiliation_merit-1.0_0.0']
 adept_af_ps_alignment_targets = ['Feb2026-AF4-PS2', 'Feb2026-AF7-PS6']
+adept_af_ss_alignment_targets = ['June2026-AF4-SS1', 'June2026-AF6-SS7']
 adept_mf_ss_alignment_targets = ['Feb2026-MF4-SS3', 'Feb2026-MF7-SS7']
 # ADEPT group targets
 #adept_mj_alignment_targets = ['ADEPT-DryRun-Moral judgement-Group-Low', 'ADEPT-DryRun-Moral judgement-Group-High']
@@ -39,6 +40,7 @@ ADEPT_SS_ALIGNMENT = random.choice(adept_ss_alignment_targets)
 ADEPT_PS_ALIGNMENT = random.choice(adept_ps_alignment_targets)
 ADEPT_MF_AF_ALIGNMENT = random.choice(adept_mf_af_alignment_targets)
 ADEPT_AF_PS_ALIGNMENT = random.choice(adept_af_ps_alignment_targets)
+ADEPT_AF_SS_ALIGNMENT = random.choice(adept_af_ss_alignment_targets)
 ADEPT_MF_SS_ALIGNMENT = random.choice(adept_mf_ss_alignment_targets)
 
 class ScenarioRunner(ABC):
